b6fb7885f616ce3ef196a8b338d3014579b3a958
Bruce Becker <brucellino@gmail.com>
2016-07-04 12:01:51 +0200

    Added the build/test/deploy scripts for CODE-RADE and updated README to reflect this.

---
+++ CODE-RADE/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+. /etc/profile.d/modules.sh
+module add ci
+module add ncurses
+module add gcc/5.2.0
+module add  openmpi/1.8.8-gcc-${GCC_VERSION}
+module add lapack/3.6.0-gcc-${GCC_VERSION}
+
+echo "REPO_DIR is "
+echo $REPO_DIR
+echo "SRC_DIR is "
+echo $SRC_DIR
+echo "WORKSPACE is "
+echo $WORKSPACE
+echo "SOFT_DIR is"
+echo $SOFT_DIR
+
+mkdir -p ${WORKSPACE}
+mkdir -p ${SRC_DIR}
+mkdir -p ${SOFT_DIR}
+
+cd ../
+make
